0565ff7ac5d8a80b3e7133d37edb9fdb460916fb
lpg42 <66437906+lpg42@users.noreply.github.com>
2021-09-15 14:21:35 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,19 @@
 'use strict';
 
-/*/token is your bot client found in discord dev portal
-//key is your api key for google found in their dev console
-//replace them with your own to activate
-//install dependencies with npm install
-//then run nodemon index.js or node index.js
-*/
+
 
 const token = '<DISCORDBOTTOKENGOESHERE>';
 const key = '<YOUTUBEAPIKEYGOESHERE>';
 const prefix = "/";
 
-/*///////////////////////////////////////////////////////////
- this bots core function was deprecated. I'd say not only is this non functional, but it probably doesn't work if you try to run it. the library it runs on got weird on us
- 
- lets be honest though do you really want to even look at a JavaScript promise? its not to late to stop scrolling lol
-/////////////////////////////////////////////////////////*/
 
-//requirements for youtube api and discord api
 const YouTube = require('simple-youtube-api');
 const youtube = new YouTube(key);
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const ytdl = require('ytdl-core');
 
-//global variables
+
 const play = prefix+'play';
 const stop = prefix+'stop';
 
@@ -53,7 +42,7 @@ bot.on('message', message => {
 	else if (message.content.startsWith(play)) {	
 	let searchQuery = message.content.slice(play.length);
   
-  //play function is here. uses promises and nesting. if you have a suggestion on how to clean this up i am open to it!
+  //play function is here. uses promises 
 	function playSong() {
 		youtube.searchVideos(searchQuery, 4)
 		.then(function(results) {	
